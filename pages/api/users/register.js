840af9501b9efde7f8d1e70a5c728f979aae523d
sachin380 <sachin2sharma001@gmail.com>
2023-01-25 03:47:51 +0530

    111

--- pages/api/users/register.js
+++ pages/api/users/register.js
@@ -19,7 +19,13 @@ export default async function register(req, res) {
             subject: "User verification link",
             url: `${process.env.SITE_URL}/api/users/verifyUser?name=${req.body.name}&email=${req.body.email}&password=${req.body.password}`,
             message: `${process.env.SITE_URL}/api/users/verifyUser?name=${req.body.name}&email=${req.body.email}&password=${req.body.password}`,
-        });
+        })
+            .then((data) => {
+                console.log("data");
+            })
+            .catch((error) => {
+                res.status(400).json({ message: error });
+            });
         res.status(200).json({ status: "success" });
     }
 }
--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,5 @@
 const nodemailer = require("nodemailer");
 const path = require("path");
-// var hbs = require("nodemailer-express-handlebars");
 import ejs from "ejs";
 
 const sendEmail = async (options) => {
@@ -11,17 +10,6 @@ const sendEmail = async (options) => {
             pass: "fqjxppcdhntarlfa",
         },
     });
-    // const handlebarOptions = {
-    //     viewEngine: {
-    //         extName: ".handlebars",
-    //         partialsDir: path.resolve("./views"),
-    //         defaultLayout: false,
-    //     },
-    //     viewPath: path.resolve("./views"),
-    //     extName: ".handlebars",
-    // };
-    // transporter.use("compile", hbs(handlebarOptions));
-    // const templatePath = path.join(__dirname, "./views/email.ejs");
     console.log(options.url);
     const data = await ejs.renderFile("./views/email.ejs", {
         name: options.name,
@@ -31,14 +19,7 @@ const sendEmail = async (options) => {
         from: "sachin sharma <sachin2sharma001@gmail.com>",
         to: options.email,
         subject: options.subject,
-        template: "email",
-        // text: options.message,
         html: data,
-        // context: {
-        //     name: `${options.name}`,
-        //     url: `${options.url}`,
-        //     email: `${options.email}`,
-        // },
     };
     await transporter.sendMail(mailOptions);
 };

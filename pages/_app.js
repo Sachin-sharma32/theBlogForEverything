89e4d50088bbffddb7f7126e9693998ca9963294
sachin380 <sachin2sharma001@gmail.com>
2023-01-24 23:27:36 +0530

    104

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,25 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import Footer from "../components/Footer";
 import { SessionProvider } from "next-auth/react";
 import Social from "../utils/Socials";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     const queryClient = new QueryClient();
 
     return (
         <div className=" w-[100%] h-[100%]">
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-PKPENVER0M"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-PKPENVER0M');
+    `}
+            </Script>
             <SessionProvider session={session}>
                 <QueryClientProvider client={queryClient}>
                     <Provider store={store}>

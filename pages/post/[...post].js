a025f60aba5ce27a320b080fe01d6423a94bf549
sachin380 <sachin2sharma001@gmail.com>
2023-01-28 12:46:00 +0530

    h

--- pages/post/[...post].js
+++ pages/post/[...post].js
@@ -168,7 +168,7 @@ const Post = ({ headerPost }) => {
                                 : `${headerPost.title}`
                         }`}
                     />
-                    <meta property="og:title" content={headerPost.title} />
+                    <meta property="og:title" content={headerPost?.title} />
                     <meta
                         name="og:description"
                         content={`${

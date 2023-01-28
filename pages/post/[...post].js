5776aa420ad158a4fe614f42e75b205f0223bb23
sachin380 <sachin2sharma001@gmail.com>
2023-01-28 13:08:38 +0530

    k

--- pages/post/[...post].js
+++ pages/post/[...post].js
@@ -22,7 +22,7 @@ import RichTextComponent from "../../components/RichTextComponent";
 import { useMemo } from "react";
 import ErrorBoundry from "../../utils/ErrorBoundry";
 
-const Post = ({ headerPost }) => {
+const Post = () => {
     const mode = useSelector((state) => state.base.mode);
     const router = useRouter();
     const posts = useSelector((state) => state.base.posts);
@@ -154,69 +154,85 @@ const Post = ({ headerPost }) => {
         return (
             <>
                 <Head>
-                    <title>TBFE - {`${headerPost?.title}`}</title>
+                    <title>TBFE - {post.title}</title>
                     <link
                         rel="icon"
-                        type="image/png"
+                        type="image/jpg"
                         href="/site-light-chopped.jpg"
                     />
                     <meta
                         name="description"
-                        content={`${
-                            post.summery
-                                ? `${headerPost.summery[0].children[0].text}`
-                                : `${headerPost.title}`
-                        }`}
+                        content='A comprehensive blogging platform that provides readers with
+                    a wide range of information on a variety of topics. From the
+                    latest news and current events, to lifestyle and personal
+                    development, the platform aims to be a one-stop-shop for all
+                    things related to blogging. Whether you&apos;re looking to
+                    stay informed, learn something new, or simply be
+                    entertained, "The Blog for Everything" has
+                    something for everyone.'
                     />
+                    <meta name="keywords" content="blog" />
                     <meta
                         property="og:title"
-                        content="The Blog For Everything"
+                        content="TBFE -  A comprehensive blogging platform that provides readers with
+                    a wide range of information on a variety of topics"
                     />
                     <meta
-                        name="og:description"
-                        content={`${
-                            headerPost.summery
-                                ? `${headerPost.summery[0].children[0].text}`
-                                : `${headerPost.title}`
-                        }`}
+                        property="og:description"
+                        content=" A comprehensive blogging platform that provides readers with
+                    a wide range of information on a variety of topics. From the
+                    latest news and current events, to lifestyle and personal
+                    development, the platform aims to be a one-stop-shop for all
+                    things related to blogging. Whether you're looking to
+                    stay informed, learn something new, or simply be
+                    entertained, 'The Blog for Everything' has
+                    something for everyone."
                     />
-                    <meta property="og:type" content="article" />
+                    <meta property="og:type" content="website" />
                     <meta
                         property="og:site_name"
                         content="TheBlogForEverything"
                     />
                     <meta
                         property="og:image"
-                        content={`${imageBuilder(headerPost.image)}`}
+                        content="https://cdn.sanity.io/images/k0me7ccv/production/8fa01467c0ac00d838090a47782c009153f72a94-1024x1024.jpg"
                     />
+                    <meta property="og:image:type" content="image/jpeg" />
+                    <meta property="og:locale" content="en_IN" />
+                    <meta
+                        property="og:image:alt"
+                        content="TheBlogForEverything logo"
+                    />
+                    <meta property="og:image:width" content="400" />
+                    <meta property="og:image:height" content="400" />
                     <meta
                         property="og:url"
-                        content={`https://www.theblogforeverything.com/headerPost/${headerPost?._id}`}
+                        content="https://www.theblogforeverything.com/"
                     />
                     <meta name="twitter:card" content="summary_large_image" />
-                    <meta
-                        name="twitter:site"
-                        content="FBFE - The Blog For Everything"
-                    />
+                    <meta name="twitter:site" content="@TBFEpage" />
                     <meta
                         name="twitter:title"
-                        content={`${headerPost.title}`}
+                        content="The Blog For Everything"
                     />
                     <meta
                         name="twitter:description"
-                        content={`${
-                            headerPost.summery
-                                ? `${headerPost.summery[0].children[0].text}`
-                                : `${headerPost.title}`
-                        }`}
+                        content="A comprehensive blogging platform that provides readers with
+                    a wide range of information on a variety of topics. From the
+                    latest news and current events, to lifestyle and personal
+                    development, the platform aims to be a one-stop-shop for all
+                    things related to blogging. Whether you're looking to
+                    stay informed, learn something new, or simply be
+                    entertained, 'The Blog for Everything' has
+                    something for everyone."
                     />
                     <meta
                         name="twitter:image"
-                        content={`${imageBuilder(headerPost.image)}`}
+                        content="https://cdn.sanity.io/images/k0me7ccv/production/8fa01467c0ac00d838090a47782c009153f72a94-1024x1024.jpg"
                     />
                     <meta
                         name="og:url"
-                        content={`https://www.theblogforeverything.com/headerPost${headerPost._id}`}
+                        content="https://www.theblogforeverything.com/"
                     />
                 </Head>
 
@@ -594,15 +610,15 @@ const Post = ({ headerPost }) => {
 
 export default Post;
 
-export async function getServerSideProps(context) {
-    const { query } = context;
-    const headerPost = await client.fetch(
-        `*[_type == "post" && _id == $id][0]`,
-        {
-            id: query.post[0],
-        }
-    );
-    return {
-        props: { headerPost },
-    };
-}
+// export async function getServerSideProps(context) {
+//     const { query } = context;
+//     const headerPost = await client.fetch(
+//         `*[_type == "post" && _id == $id][0]`,
+//         {
+//             id: query.post[0],
+//         }
+//     );
+//     return {
+//         props: { headerPost },
+//     };
+// }
